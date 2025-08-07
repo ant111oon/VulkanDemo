6f45bd2ef79c1ec5cf68220ea21bd88d4d47ba93
Anton Moyseychuk <moyseychuk2017@mail.ru>
2025-08-07 23:36:49 +0400

    - Initialized device

--- source/main.cpp
+++ source/main.cpp
@@ -70,6 +70,31 @@ bool CheckVkInstLayersSupport(const std::span<const char* const> requiredLayers)
 }
 
 
+bool CheckVkDeviceExtensionsSupport(VkPhysicalDevice vkPhysDevice, const std::span<const char* const> requiredExtensions)
+{
+#ifdef ENG_BUILD_DEBUG
+    uint32_t vkDeviceExtensionsCount = 0;
+    VK_CHECK(vkEnumerateDeviceExtensionProperties(vkPhysDevice, nullptr, &vkDeviceExtensionsCount, nullptr));
+    std::vector<VkExtensionProperties> vkDeviceExtensionProps(vkDeviceExtensionsCount);
+    VK_CHECK(vkEnumerateDeviceExtensionProperties(vkPhysDevice, nullptr, &vkDeviceExtensionsCount, vkDeviceExtensionProps.data()));
+
+    for (const char* pExtensionName : requiredExtensions) {
+        const auto reqLayerIt = std::find_if(vkDeviceExtensionProps.cbegin(), vkDeviceExtensionProps.cend(), [&](const VkExtensionProperties& props) {
+            return strcmp(pExtensionName, props.extensionName) == 0;
+        });
+        
+        if (reqLayerIt == vkDeviceExtensionProps.cend()) {
+            return false;
+        }
+    }
+
+    return true;
+#else
+    return true;
+#endif
+}
+
+
 #ifdef ENG_BUILD_DEBUG
 static VkBool32 VKAPI_PTR DbgVkMessageCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
@@ -88,7 +113,7 @@ static VkBool32 VKAPI_PTR DbgVkMessageCallback(
             pType = "GENERAL";
             break;
         case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
-             pType = "VALIDATION";
+            pType = "VALIDATION";
             break;
         case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
             pType = "PERFORMANCE";
@@ -146,16 +171,14 @@ VkInstance InitVkInstance(const char* pAppName)
     };
 
     VK_ASSERT_MSG(CheckVkInstExtensionsSupport(vkInstExtensions), "Not all required instance extensions are supported");
-    
+
 #ifdef ENG_BUILD_DEBUG
-    constexpr std::array vkInstLayers = {
+     constexpr std::array vkInstLayers = {
         "VK_LAYER_KHRONOS_validation",
     };
     
     VK_ASSERT_MSG(CheckVkInstLayersSupport(vkInstLayers), "Not all required instance layers are supported");
-#endif
 
-#ifdef ENG_BUILD_DEBUG
     VkDebugUtilsMessengerCreateInfoEXT vkDbgMessengerCreateInfo = {};
     vkDbgMessengerCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
     vkDbgMessengerCreateInfo.messageType = 
@@ -252,10 +275,83 @@ VkPhysicalDevice InitVkPhysDevice(VkInstance vkInstance)
 
 VkDevice InitVkDevice(VkPhysicalDevice vkPhysDevice)
 {
-    VkDeviceCreateInfo createInfo = {};
-    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    uint32_t queueFamilyPropsCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysDevice, &queueFamilyPropsCount, nullptr);
+    
+    std::vector<VkQueueFamilyProperties> queueFamilyProps(queueFamilyPropsCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysDevice, &queueFamilyPropsCount, queueFamilyProps.data());
+
+    uint32_t graphicsQueueFamilyIndex = UINT32_MAX;
+    uint32_t computeQueueFamilyIndex = UINT32_MAX;
+    uint32_t transferQueueFamilyIndex = UINT32_MAX;
+
+    auto IsQueueFamilyIndexValid = [](uint32_t index) -> bool { return index != UINT32_MAX; };
+
+    for (uint32_t i = 0; i < queueFamilyProps.size(); ++i) {
+        const VkQueueFamilyProperties& props = queueFamilyProps[i];
+
+        if (!IsQueueFamilyIndexValid(graphicsQueueFamilyIndex) && (props.queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
+            graphicsQueueFamilyIndex = i;
+        }
+
+        if (!IsQueueFamilyIndexValid(computeQueueFamilyIndex) && (props.queueFlags & VK_QUEUE_COMPUTE_BIT)) {
+            computeQueueFamilyIndex = i;
+        }
+
+        if (!IsQueueFamilyIndexValid(transferQueueFamilyIndex) && (props.queueFlags & VK_QUEUE_TRANSFER_BIT)) {
+            transferQueueFamilyIndex = i;
+        }
+
+        if (IsQueueFamilyIndexValid(graphicsQueueFamilyIndex) && 
+            IsQueueFamilyIndexValid(computeQueueFamilyIndex) && 
+            IsQueueFamilyIndexValid(transferQueueFamilyIndex)
+        ) {
+            break;
+        }
+    }
+
+    VK_ASSERT_MSG(IsQueueFamilyIndexValid(graphicsQueueFamilyIndex), "Failed to get graphics queue family index");
+    VK_ASSERT_MSG(IsQueueFamilyIndexValid(computeQueueFamilyIndex), "Failed to get compute queue family index");
+    VK_ASSERT_MSG(IsQueueFamilyIndexValid(transferQueueFamilyIndex), "Failed to get transfer queue family index");
+
+    VK_ASSERT_MSG(graphicsQueueFamilyIndex == computeQueueFamilyIndex && computeQueueFamilyIndex == transferQueueFamilyIndex,
+        "Queue family indices for graphics, compute and transfer must be equal, for now. TODO: process the case when they are different");
+
+    VkDeviceQueueCreateInfo queueCreateInfo = {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = graphicsQueueFamilyIndex;
+    queueCreateInfo.queueCount = 1;
+    
+    const float queuePriority = 1.f;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    VkDeviceCreateInfo deviceCreateInfo = {};
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCreateInfo.queueCreateInfoCount = 1;
+    deviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
+
+    constexpr std::array deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
+    };
+
+    VK_ASSERT(CheckVkDeviceExtensionsSupport(vkPhysDevice, deviceExtensions));
+
+    deviceCreateInfo.enabledExtensionCount = deviceExtensions.size();
+    deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
+    VkPhysicalDeviceDynamicRenderingFeatures dynRenderingFeature = {};
+    dynRenderingFeature.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DYNAMIC_RENDERING_FEATURES;
+    dynRenderingFeature.dynamicRendering = VK_TRUE;
+
+    VkPhysicalDeviceFeatures2 features2 = {};
+    features2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+    features2.pNext = &dynRenderingFeature;
+
+    deviceCreateInfo.pNext = &features2;
 
     VkDevice device = VK_NULL_HANDLE;
+    VK_CHECK(vkCreateDevice(vkPhysDevice, &deviceCreateInfo, nullptr, &device));
 
     return device;
 }
@@ -287,6 +383,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    vkDestroyDevice(vkDevice, nullptr);
     vkDestroySurfaceKHR(vkInstance, vkSurface, nullptr);
     
     // NOTE: Debug messenger is automatically created and destroyed by the Vulkan loader via pNext.
