a3dea48e868b1f3640f4680a4187f6d90338820d
Anton Moyseychuk <moyseychuk2017@mail.ru>
2026-08-22 22:22:47 +0400

    - Refactored shader binary generation

--- source/main.cpp
+++ source/main.cpp
@@ -4326,34 +4326,66 @@ static void CreatePipelines()
     CreateDbgRTViewPipelineLayout();
 
 
-    CreateGeomCullingPipeline(RND_SHADER_SPIRV_FULL_PATH("geom_culling.cs.spv"));
-    CreateGeomBatchingPipeline(RND_SHADER_SPIRV_FULL_PATH("geom_batching.cs.spv"));
-    CreateGeomDrawCmdGenPipeline(RND_SHADER_SPIRV_FULL_PATH("geom_draw_cmd_gen.cs.spv"));
+    CreateGeomCullingPipeline(RND_SHADER_SPIRV_FULL_PATH("geom/geom_culling.cs.spv"));
+    CreateGeomBatchingPipeline(RND_SHADER_SPIRV_FULL_PATH("geom/geom_batching.cs.spv"));
+    CreateGeomDrawCmdGenPipeline(RND_SHADER_SPIRV_FULL_PATH("geom/geom_draw_cmd_gen.cs.spv"));
     
-    CreateZPassPipeline(RND_SHADER_SPIRV_FULL_PATH("zpass.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("zpass.ps.spv"));
+    CreateZPassPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("zpass/zpass.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("zpass/zpass.ps.spv")
+    );
     
-    CreateHZBGenPipeline(RND_SHADER_SPIRV_FULL_PATH("hzb.cs.spv"));
+    CreateHZBGenPipeline(RND_SHADER_SPIRV_FULL_PATH("hzb/hzb.cs.spv"));
 
-    CreateCSMGeomCullingPipeline(RND_SHADER_SPIRV_FULL_PATH("csm.cs.spv"));
-    CreateCSMRenderPipeline(RND_SHADER_SPIRV_FULL_PATH("csm.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("csm.ps.spv"));
+    CreateCSMGeomCullingPipeline(RND_SHADER_SPIRV_FULL_PATH("csm/csm.cs.spv"));
+    CreateCSMRenderPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("csm/csm.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("csm/csm.ps.spv")
+    );
     
-    CreateGBufferRenderPipeline(RND_SHADER_SPIRV_FULL_PATH("gbuffer.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("gbuffer.ps.spv"));
+    CreateGBufferRenderPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("gbuffer/gbuffer.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("gbuffer/gbuffer.ps.spv")
+    );
     
-    CreateDeferredLightingPipeline(RND_SHADER_SPIRV_FULL_PATH("deferred_lighting.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("deferred_lighting.ps.spv"));
+    CreateDeferredLightingPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("deferred_lighting/deferred_lighting.vs.spv"), 
+        RND_SHADER_SPIRV_FULL_PATH("deferred_lighting/deferred_lighting.ps.spv")
+    );
     
-    CreatePostProcessingPipeline(RND_SHADER_SPIRV_FULL_PATH("post_processing.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("post_processing.ps.spv"));
+    CreatePostProcessingPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("post_processing/post_processing.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("post_processing/post_processing.ps.spv")
+    );
     
-    CreateBackbufferPassPipeline(RND_SHADER_SPIRV_FULL_PATH("backbuffer.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("backbuffer.ps.spv"));
+    CreateBackbufferPassPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("backbuffer/backbuffer.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("backbuffer/backbuffer.ps.spv")
+    );
     
-    CreateSkyboxPipeline(RND_SHADER_SPIRV_FULL_PATH("skybox.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("skybox.ps.spv"));
+    CreateSkyboxPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("skybox/skybox.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("skybox/skybox.ps.spv")
+    );
     
-    CreateIrradianceMapGenPipeline(RND_SHADER_SPIRV_FULL_PATH("irradiance_map_gen.cs.spv"));
-    CreatePrefilteredEnvMapGenPipeline(RND_SHADER_SPIRV_FULL_PATH("prefiltered_env_map_gen.cs.spv"));
-    CreateBRDFIntegrationLUTGenPipeline(RND_SHADER_SPIRV_FULL_PATH("brdf_integration_gen.cs.spv"));
+    CreateIrradianceMapGenPipeline(RND_SHADER_SPIRV_FULL_PATH("IBL/irradiance_map_gen.cs.spv"));
+    CreatePrefilteredEnvMapGenPipeline(RND_SHADER_SPIRV_FULL_PATH("IBL/prefiltered_env_map_gen.cs.spv"));
+    CreateBRDFIntegrationLUTGenPipeline(RND_SHADER_SPIRV_FULL_PATH("IBL/brdf_integration_gen.cs.spv"));
     
-    CreateDbgDrawLinePipeline(RND_SHADER_SPIRV_FULL_PATH("dbg_draw_lines.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("dbg_draw_lines.ps.spv"));
-    CreateDbgDrawTrianglePipeline(RND_SHADER_SPIRV_FULL_PATH("dbg_draw_triangles.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("dbg_draw_triangles.ps.spv"));
-    CreateDbgRTViewPipeline(RND_SHADER_SPIRV_FULL_PATH("dbg_rt_view.vs.spv"), RND_SHADER_SPIRV_FULL_PATH("dbg_rt_view.ps.spv"));
+    CreateDbgDrawLinePipeline(
+        RND_SHADER_SPIRV_FULL_PATH("dbg_draw_primitives/dbg_draw_lines.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("dbg_draw_primitives/dbg_draw_lines.ps.spv")
+    );
+
+    CreateDbgDrawTrianglePipeline(
+        RND_SHADER_SPIRV_FULL_PATH("dbg_draw_primitives/dbg_draw_triangles.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("dbg_draw_primitives/dbg_draw_triangles.ps.spv")
+    );
+
+    CreateDbgRTViewPipeline(
+        RND_SHADER_SPIRV_FULL_PATH("dbg_rt_view/dbg_rt_view.vs.spv"),
+        RND_SHADER_SPIRV_FULL_PATH("dbg_rt_view/dbg_rt_view.ps.spv")
+    );
 }
 
 
